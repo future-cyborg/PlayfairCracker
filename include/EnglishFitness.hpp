eeca3e7b6f3c612fbf383c70bc0ba6c803edcbde
Unknown <valetudinarygoat@gmail.com>
2018-05-01 13:30:02 -0700

    Document EnglishFitness

--- include/EnglishFitness.hpp
+++ include/EnglishFitness.hpp
@@ -19,13 +19,49 @@
 
 typedef double score_t;
 
+/**
+ * @brief Rate how likely a text is English
+ * 
+ * Rate the fitness of how likely a text is English, or given frequencies.
+ * 
+ * @param standardFreq The standard n-gram frequencies the fitnessfunction is compared against
+ */
 class EnglishFitness {
 public:
+	/**
+	 * @param standardFreq  The standard frequencies the fitnessfunction is compared against
+	 */
 	EnglishFitness(const FrequencyCollector &standardFreq);
 	~EnglishFitness();
 
+	/**
+	 * @brief Collect fitness score for given n-gram frequencies
+	 * 
+	 * Collect fitness score for given n-gram frequencies.
+	 * 
+	 * @param testFreq  The test frequencies fintess is collected for
+	 * @return score_t
+	 */
 	score_t fitness(const FrequencyCollector &testFreq) const;
+	/**
+	 * @brief Return n-gram size associated with this object
+	 * 
+	 * Return n-gram size associated with this object.
+	 * 
+	 * @return unsigned int
+	 */
 	unsigned getN() const;
+	/**
+	 * @brief Return the maximum fitness score possible
+	 * 
+	 * Return the maximum fitness score possible for this set of frequencies.
+	 * 	Defined by
+	 * 	
+	 * 	( max(standardFreq, testFreq)^2 ) / 2
+	 * 
+	 * @param testFreq The test frequency maxFitness is collected for
+	 * @return score_t
+	 */
 	score_t maxFitness(const FrequencyCollector &testFreq) const;
 
 private:
